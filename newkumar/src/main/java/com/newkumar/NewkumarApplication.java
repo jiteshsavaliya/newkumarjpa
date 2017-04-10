1b3b955a49909f50bdb74e567f15acb558a25112
jitesh <jitesh@jitesh-pc>
2017-04-10 11:40:30 -0700

    init `12

---
+++ newkumar/src/main/java/com/newkumar/NewkumarApplication.java
@@ -0,0 +1,12 @@
+package com.newkumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewkumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewkumarApplication.class, args);
+	}
+}
