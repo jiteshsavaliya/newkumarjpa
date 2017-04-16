ea2bab11e25b272769ca3420adacb6450be1b28b
jitesh <jitesh@jitesh-pc>
2017-04-16 02:56:55 -0700

    first

---
+++ workspace/newkumarjpa/src/main/java/com/example/NewkumarjpaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewkumarjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewkumarjpaApplication.class, args);
+	}
+}
